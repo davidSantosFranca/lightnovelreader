3503554e9931002f5ae3a01bc109a9eef90640c9
davidSantosFranca <david1995sf@gmail.com>
2024-02-21 23:42:46 -0300

    fix

--- src/components/ChaptersDropdown.tsx
+++ src/components/ChaptersDropdown.tsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
 import { useChaptersList } from "../hooks/useFiles";
-import { getCurrentChapter } from "../utils/localStorageUtils";
 
 export const ChaptersDropdown = ({current, series, setCurrentChapter}:{current?:string, series:"tsukimichi", setCurrentChapter:(name:string)=>void}) => {
     const data = useChaptersList(series);
-    useEffect(() => {
-        if (!current && data.length > 0) {
-            setCurrentChapter(getCurrentChapter()[0] ?? data[0]);
-        }
-    }, [current, data, setCurrentChapter]);
-    //dropdown for chapters
+    
     return (
         <select id="dropdown" value={current} onChange={(e)=>{
             setCurrentChapter(e.currentTarget.value);
